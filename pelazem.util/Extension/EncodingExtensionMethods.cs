56044827156d6650eb3f3c60789723b9497212c4
Patrick El-Azem <paelaz@microsoft.com>
2018-12-22 16:04:22 -0500

    Add encoding extension methods

---
+++ pelazem.util/Extension/EncodingExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pelazem.util
+{
+	public static class EncodingExtensionMethods
+	{
+		public static string EncodeBase64(this Encoding encoding, string text)
+		{
+			if (string.IsNullOrWhiteSpace(text))
+				return string.Empty;
+
+			byte[] textAsBytes = encoding.GetBytes(text);
+
+			return Convert.ToBase64String(textAsBytes);
+		}
+
+		public static string DecodeBase64(this Encoding encoding, string encodedText)
+		{
+			if (string.IsNullOrWhiteSpace(encodedText))
+				return string.Empty;
+
+			byte[] textAsBytes = Convert.FromBase64String(encodedText);
+
+			return encoding.GetString(textAsBytes);
+		}
+	}
+}
