f2284ee4dddaa6c2d64bfd54f5f566f256e166b4
Patrick El-Azem <paelaz@microsoft.com>
2018-12-29 12:05:02 -0500

    Add encoding extension methods

---
+++ pelazem.util.tests/EncodingExtensionMethodTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using pelazem.util;
+
+namespace pelazem.util.tests
+{
+	public class EncodingExtensionMethodTests
+	{
+		private static string _rawString = "D/General/Active-Special_Servicing/2011/WFRBS_2011-C4/Closing_Binder/22._WFB_–_Master_Servicer_-_Indemnification_Agreement.pdf";
+		private static string _encodedStringSafe = "RC9HZW5lcmFsL0FjdGl2ZS1TcGVjaWFsX1NlcnZpY2luZy8yMDExL1dGUkJTXzIwMTEtQzQvQ2xvc2luZ19CaW5kZXIvMjIuX1dGQl_igJNfTWFzdGVyX1NlcnZpY2VyXy1fSW5kZW1uaWZpY2F0aW9uX0FncmVlbWVudC5wZGY";
+		private static string _encodedStringUnsafe = "RC9HZW5lcmFsL0FjdGl2ZS1TcGVjaWFsX1NlcnZpY2luZy8yMDExL1dGUkJTXzIwMTEtQzQvQ2xvc2luZ19CaW5kZXIvMjIuX1dGQl/igJNfTWFzdGVyX1NlcnZpY2VyXy1fSW5kZW1uaWZpY2F0aW9uX0FncmVlbWVudC5wZGY=";
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData("")]
+		public void EmptyShouldReturnEmptyString(string value)
+		{
+			// Arrange
+
+			// Act
+			string result = Encoding.UTF8.EncodeBase64(value);
+
+			// Assert
+			Assert.True(result.Length == 0);
+		}
+
+		[Fact]
+		public void RawShouldEncodeSafe()
+		{
+			// Arrange
+
+			// Act
+			string encoded = Encoding.UTF8.EncodeBase64(_rawString, true);
+
+			// Assert
+			Assert.Equal(_encodedStringSafe, encoded);
+		}
+
+		[Fact]
+		public void RawShouldEncodeUnsafe()
+		{
+			// Arrange
+
+			// Act
+			string encoded = Encoding.UTF8.EncodeBase64(_rawString, false);
+
+			// Assert
+			Assert.Equal(_encodedStringUnsafe, encoded);
+		}
+
+		[Fact]
+		public void SafeShouldDecodeRaw()
+		{
+			// Arrange
+
+			// Act
+			string raw = Encoding.UTF8.DecodeBase64(_encodedStringSafe, true);
+
+			// Assert
+			Assert.Equal(_rawString, raw);
+		}
+
+		[Fact]
+		public void UnsafeShouldDecodeRawEvenSafe()
+		{
+			// Arrange
+
+			// Act
+			string raw = Encoding.UTF8.DecodeBase64(_encodedStringUnsafe, true);
+
+			// Assert
+			Assert.Equal(_rawString, raw);
+		}
+
+		[Fact]
+		public void UnsafeShouldDecodeRaw()
+		{
+			// Arrange
+
+			// Act
+			string raw = Encoding.UTF8.DecodeBase64(_encodedStringUnsafe, false);
+
+			// Assert
+			Assert.Equal(_rawString, raw);
+		}
+	}
+}
--- pelazem.util/Extension/EncodingExtensionMethods.cs
+++ pelazem.util/Extension/EncodingExtensionMethods.cs
@@ -6,24 +6,82 @@ namespace pelazem.util
 {
 	public static class EncodingExtensionMethods
 	{
-		public static string EncodeBase64(this Encoding encoding, string text)
+		private static readonly char paddingChar = '=';
+		static readonly char[] paddingChars = { paddingChar };
+		private static readonly string paddingString = new string(paddingChar, 1);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="encoding"></param>
+		/// <param name="text"></param>
+		/// <param name="makeFileUrlPathSafe">If true, cleans the result as follows: 1. strips padding at end | 2. replaces + (plus) with - (minus) | 3. Replaces / (slash) with _ (underscore)</param>
+		/// <returns></returns>
+		public static string EncodeBase64(this Encoding encoding, string text, bool makeFileUrlPathSafe = false)
 		{
 			if (string.IsNullOrWhiteSpace(text))
 				return string.Empty;
 
 			byte[] textAsBytes = encoding.GetBytes(text);
 
-			return Convert.ToBase64String(textAsBytes);
+			string result = Convert.ToBase64String(textAsBytes);
+
+			if (makeFileUrlPathSafe)
+			{
+				result = result
+				.TrimEnd(paddingChars)
+				.Replace('+', '-')
+				.Replace('/', '_')
+				;
+			}
+
+			return result;
 		}
 
-		public static string DecodeBase64(this Encoding encoding, string encodedText)
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="encoding"></param>
+		/// <param name="encodedText"></param>
+		/// <param name="reverseFileUrlPathSafe">If true, reverses the safety measures optionally added by EncodeBase64() by adding back padding, replacing - with +, and replacing _ with / prior to decoding.</param>
+		/// <returns></returns>
+		public static string DecodeBase64(this Encoding encoding, string encodedText, bool reverseFileUrlPathSafe = false)
 		{
+			byte[] bytes;
+			string result = string.Empty;
+
 			if (string.IsNullOrWhiteSpace(encodedText))
-				return string.Empty;
+				return result;
 
-			byte[] textAsBytes = Convert.FromBase64String(encodedText);
+			if (!reverseFileUrlPathSafe)
+				bytes = Convert.FromBase64String(encodedText);
+			else
+			{
+				string raw = encodedText
+					.Replace('-', '+')
+					.Replace('_', '/')
+				;
 
-			return encoding.GetString(textAsBytes);
+				// Add padding at end if not there already
+				if (!raw.EndsWith(paddingString))
+				{
+					switch (raw.Length % 4)
+					{
+						case 2:
+							raw += new string(paddingChar, 2);
+							break;
+						case 3:
+							raw += paddingString;
+							break;
+					}
+				}
+
+				bytes = Convert.FromBase64String(raw);
+			}
+
+			result = encoding.GetString(bytes);
+
+			return result;
 		}
 	}
 }
